002c85a7d6ecf14ff42a19fe305636147894b4d3
RaunakAgarwal18 <raunak180803@gmail.com>
2022-12-07 17:50:54 +0530

    footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import '../CSS/Footer.css'
+function Footer() {
+  return (
+    <div style={{
+        backgroundColor: 'black',
+        color: 'white'
+    }} className='footer'>
+        <div>
+            <ul>
+                <li>EVENTS</li>
+                <li>MERCH</li>
+                <li>CAMPAIGNS</li>
+                <li>TEAMS</li>
+                <li>MUN</li>
+                <li>SPONSORS</li>
+                <li>CONTESTS</li>
+            </ul>
+        </div>
+        <div>HAIL <br></br> ALCHER!!</div>
+        <div>
+            <div>
+                Shareka Iqbal <br></br>
+                PR HEAD<br></br>
+                +91 8919054239
+            </div>
+            <div>
+                Abhishek Singh<br></br>
+                PR HEAD<br></br>
+                +91 9983072631
+            </div>
+        </div>
+        <div>Alcher Logo</div>
+        <div>
+            <a href=''></a>
+            <a href=''></a>
+            <a href=''></a>
+            <a href=''></a>
+            For Business Inqueries: <b>alcheringa@iitg.ac.in</b>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
