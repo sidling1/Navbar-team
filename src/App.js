41dbf0f52abd2678fa7427aa96cb1f900ba7e6e6
sidjethwani@gmail.com <sidjethwani@gmail.com>
2022-12-11 01:12:09 +0530

    message

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'           element = {<Home />}/>
+        <Route path='*'           element = {<Home />}/>
         <Route path='Events'      element = {<Events />}/>
         <Route path='Merch'       element = {<Merch />}/>
         <Route path='Campaigns'   element = {<Campaigns />}/>
