2ee65725a1bd90b8c53018021d0662ba964d59f3
Soumya-002 <108822732+Soumya-002@users.noreply.github.com>
2022-12-11 23:30:23 +0530

    content uploaded

---
+++ src/Components/content.js
@@ -0,0 +1,113 @@
+const content = () => {
+    return ( <div className="container" >
+        
+    <div className="rect" >
+          <div className="bg" >
+            <p className="para" > WE 'VE WORKED WITH</p>
+  </div></div >
+
+        
+  <div className="layout">
+    
+              <div className="box1" >
+                    <div className="frame" >
+                          <p className="para1" > Title sponsor </p> 
+                                </div>
+
+  <div className="box2" >    
+                    <div className="ibox" > </div>
+  <p className="oil" > Indian oil </p>
+              </div></div >
+
+                  
+  <div className="cosponsor" >
+                        <div className="co1" >                   
+                            <div className="copara" > Co Title Sponsors </div>                
+                            </div>  
+
+  <div className="parent" >              
+          <div className="cocard" >                     
+                <p className="newpara" > Indian Oil </p> 
+                        </div> 
+  <div className="cocard" >                   
+                  <p className="newpara" > Indian Oil </p> 
+                        </div> 
+  <div className="cocard" >                                 
+                  <p className="newpara" > Indian Oil </p></div > </div> 
+                        </div>
+
+
+  <div className="randomheading" >                     
+            <div className="co1" >                                  
+                  <div className="copara" > Random Heading </div> 
+                        </div>
+
+                                        
+  <div className="co-parent" >
+                                          
+              <div className="cocard" >                         
+                      <p className="newpara" > Indian Oil </p> 
+                              </div> 
+              <div className="cocard"> 
+                      <p className="newpara" > Indian Oil </p> 
+                            </div> 
+              <div className="cocard"> 
+                      <p className="newpara" > Indian Oil </p> 
+                            </div> 
+              <div className="cocard"> 
+                      <p className="newpara" > Indian Oil </p> 
+                            </div> </div>                                          
+  </div>
+  
+  
+<div className="othersponsor"> 
+      <div className="co1">
+          <div className="copara">Other Sponsors</div>
+</div>
+    <div className="random-parent">
+                  <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div> 
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div> 
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div> 
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div>
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div>
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div>
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div> 
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div>
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div> 
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div>
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div>
+                                <div className="cocard" >                         
+                          <p className="newpara" > Indian Oil </p> 
+                                </div>
+
+</div>
+</div> 
+                                                  
+</div>                                            
+                                       
+  </div>  );
+}
+ 
+export default content;
