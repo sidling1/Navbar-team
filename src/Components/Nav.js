c2789b5a78f2d8935b48d69ed3ad6b6c940ea8ed
RaunakAgarwal18 <raunak180803@gmail.com>
2022-12-12 22:14:16 +0530

    responsive nav and footer

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,19 +8,19 @@ import Navpopup from './Navpopup'
 function Nav() {
     return (
     <div>
-        <header>
+        <header className='navHeader'>
             <div><Logo/></div>
             <nav className='NavigationBar'>
                 <ul className='NavigationList'>
-                    <li id="Events"><NavLink to="/Events">EVENTS</NavLink></li>
-                    <li id="Merch"><NavLink to="/Merch">MERCH</NavLink></li>
-                    <li><NavLink to="/Campaigns">CAMPAIGNS</NavLink></li>
-                    <li><NavLink to="/Teams">TEAMS</NavLink></li>
-                    <li><NavLink to="/MUN"> MUN &emsp;</NavLink></li>
-                    <li><NavLink to="/Sponsors" style={{
+                    <li className="Events"><NavLink to="/Events">EVENTS</NavLink></li>
+                    <li className="Merch"><NavLink to="/Merch">MERCH</NavLink></li>
+                    <li className='Campaigns'><NavLink to="/Campaigns">CAMPAIGNS</NavLink></li>
+                    <li className='Teams'><NavLink to="/Teams">TEAMS</NavLink></li>
+                    <li className='Mun'><NavLink to="/MUN"> MUN &emsp;</NavLink></li>
+                    <li className='Sponsors'><NavLink to="/Sponsors" style={{
                         marginLeft : '-25px'
                         }}>SPONSORS</NavLink></li>
-                    <li><NavLink to="/Contests">CONTESTS</NavLink></li>
+                    <li className='Contests'><NavLink to="/Contests">CONTESTS</NavLink></li>
                 </ul>
             </nav>
             <Link className='image' to="/BookTickets"><Pictures /></Link>
